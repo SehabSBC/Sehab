bf40006915871f35d7146483e2dfd8567633ab16
Rafael Zinezi Araújo <rafael.araujo.icsp@gmail.com>
2018-09-10 16:45:17 -0300

    Modelagem Assentamento

--- sihisb/models.py
+++ sihisb/models.py
@@ -23,9 +23,9 @@ class DomainValue(models.Model):
 
 
 class Assentamento(models.Model):           # Caracterização
-    id_funep = models.IntegerField
+    id_funep = models.PositiveSmallIntegerField
     nm_area = models.CharField(max_length=100)
-    id_sehab = models.IntegerField
+    id_sehab = models.PositiveSmallIntegerField
     ds_regiao_op = models.CharField(max_length=5)
     dm_tipologia = models.ForeignKey(
         DomainValue,
@@ -45,14 +45,14 @@ class Assentamento(models.Model):           # Caracterização
         on_delete=models.SET_NULL
     )
     ds_localizacao = models.CharField(max_length=100)
-    vl_uhs_mapeadas = models.IntegerField
+    vl_uhs_mapeadas = models.PositiveIntegerField
 
     @property
     def _uhs_calc(self):
         return round(self.vl_uhs_mapeadas * 1.05)
 
     vl_uhs_calc = property(_uhs_calc)
-    vl_unid_cad = models.IntegerField
+    vl_unid_cad = models.PositiveIntegerField
     sn_manancial = models.BooleanField
     sn_projeto = models.BooleanField
     dm_tp_assentamento = models.ForeignKey(
@@ -64,6 +64,35 @@ class Assentamento(models.Model):           # Caracterização
         DomainValue,
         limit_choices_to={'id_domain_fk': domains.SITUA_PROPRIEDADE}
     )
+    tx_obs = models.TextField
+    dm_situa_prog = models.ManyToManyField(
+        DomainValue,
+        limit_choices_to={'id_domain_fk': domains.SITUA_PROG}
+    )
+    dm_situa_estagio = models.ForeignKey(
+        DomainValue,
+        limit_choices_to={'id_domain_fk': domains.SITUA_ESTAGIO},
+        on_delete=models.SET_NULL
+    )
+    vl_classifica = models.PositiveSmallIntegerField
+    vl_hierarq = models.PositiveSmallIntegerField
+    vl_ano_doc = models.PositiveSmallIntegerField
+    vl_ano_moradores = models.PositiveSmallIntegerField
+    dm_apps = models.ManyToManyField(
+        DomainValue,
+        limit_choices_to={'id_domain_fk': domains.APPS}
+    )
+    dm_zoneamento = models.ForeignKey(
+        DomainValue,
+        limit_choices_to={'id_domain_fk': domains.ZONEAMENTO},
+        on_delete=models.SET_NULL
+    )
+    ds_feicao_morf = models.CharField
+    ds_gravames = models.CharField
+    tx_obs_jur = models.TextField
+    tx_caracteriza = models.TextField
+    tx_gps = models.TextField                   #tem algo melhor
+    img_fotos = models.ImageField
 
     def __str__(self):
         return self.nm_area
